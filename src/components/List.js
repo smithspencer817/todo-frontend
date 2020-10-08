9b959963feb2c679a5431132507e208e6ddb0480
smithspencer817 <smithspencer817@gmail.com>
2020-10-08 11:43:17 -0500

    added delete functionality to list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { deleteList } from '../actions/lists';
+import { connect } from 'react-redux';
 
-export default function List(props) {
-    const { name } = props.list
+function List(props) {
+    const { name, id } = props.list
+
+    function handleDelete(id) {
+        console.log(id);
+        const token = document.cookie.slice(10);
+        fetch(`http://localhost:3000/api/lists/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        props.deleteList(id);
+    }
+
     return(
-        <li>{name}</li>
+        <li>{name} {id} <button onClick={() => handleDelete(id)}>X</button></li>
     )
 }
+
+export default connect(null, { deleteList })(List);
