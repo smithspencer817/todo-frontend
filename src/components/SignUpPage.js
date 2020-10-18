590eaa0853bfeb24d8afba1f8837eed916eb42aa
smithspencer817 <smithspencer817@gmail.com>
2020-10-17 19:14:31 -0500

    added sign up functionality to create new user

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addCurrentUser } from '../actions/user';
 
-export default function SignUpPage() {
+function SignUpPage(props) {
+
+    let history = useHistory();
 
     function handleNewUser(e) {
         e.preventDefault();
         const form = e.target;
         const newUserInfo = {
-          username: form[0].value,
-          password: form[1].value
+            first_name: form[0].value,
+            last_name: form[1].value,
+            username: form[2].value,
+            password: form[3].value
         }
-        console.log(newUserInfo)
+        fetch('http://localhost:3000/api/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(newUserInfo)
+        })
+        .then(res => res.json())
+        .then(res => {
+            if (res[0] !== undefined) {
+                console.log('was not valid')
+            } else {
+                fetch('http://localhost:3000/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(res)
+                })
+                .then(res => res.json())
+                .then(res => {
+                    document.cookie = `authToken=${res.token}`;
+                    props.addCurrentUser(res.user);
+                    history.push('/home');
+                });
+            }
+        })
         form.reset();
       }
 
@@ -21,6 +55,12 @@ export default function SignUpPage() {
                     <h1 id="login-title-text">Sign Up!</h1>
                 </div>
                 <Form onSubmit={(e) => handleNewUser(e)}>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="First Name" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="Last Name" />
+                    </Form.Group>
                     <Form.Group>
                         <Form.Control type="text" placeholder="Username" />
                     </Form.Group>
@@ -33,5 +73,7 @@ export default function SignUpPage() {
                 </Form>
             </div>
         </div>
-    )
-}
+    );
+};
+
+export default connect(null, { addCurrentUser })(SignUpPage);
