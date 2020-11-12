c596bf702f70424bb1500c02ae9d6a9fdee32887
smithspencer817 <smithspencer817@gmail.com>
2020-11-12 16:28:00 -0600

    changed hour styling on list items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,6 +6,13 @@ export default function ListItem(props){
     let date = new Date(props.listItem.createdAt)
     let [weekday, month, day, year, time] = date.toString().split(" ");
     time = time.slice(0,5);
+    let [hour, minute] = time.split(":");
+    let period = hour >= 12 ? "PM" : "AM"
+
+    if (parseInt(hour) > 12) {
+         hour = (parseInt(hour) - 12).toString()
+
+    }
 
     let [complete, toggleCompletion] = useState(false);
 
@@ -13,15 +20,20 @@ export default function ListItem(props){
         <div className="individual-list-item-container">
             <div className="individual-list-item-info">
                 <h4>{props.listItem.description}</h4>
-                <p>Added: {weekday} {month} {day}, {year} @ {time}</p>
+                <p>{weekday} {month} {day}, {year} @ {hour}:{minute} {period}</p>
             </div>
             <div className="individual-list-item-actions">
                 <Pencil></Pencil>
                 {
                     complete ? 
-                    <CheckCircleFill className="complete-list-item" onClick={() => toggleCompletion(false)}></CheckCircleFill>
+                    <CheckCircleFill 
+                        className="complete-list-item" 
+                        onClick={() => toggleCompletion(false)}
+                    ></CheckCircleFill>
                     :
-                    <div className="incomplete-list-item" onClick={() => toggleCompletion(true)}></div>
+                    <div className="incomplete-list-item" 
+                        onClick={() => toggleCompletion(true)}
+                    ></div>
                 }
             </div>
         </div>
