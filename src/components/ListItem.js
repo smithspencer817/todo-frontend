3cdae6c1e6f060fdc2b38f2d86a0b7e7f73c812a
smithspencer817 <smithspencer817@gmail.com>
2020-11-12 15:34:51 -0600

    stylized list items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pencil, CheckCircleFill } from 'react-bootstrap-icons';
 
 export default function ListItem(props){
 
     let date = new Date(props.listItem.createdAt)
     let [weekday, month, day, year, time] = date.toString().split(" ");
     time = time.slice(0,5);
+
+    let [complete, toggleCompletion] = useState(false);
 
     return(
         <div className="individual-list-item-container">
@@ -13,9 +16,13 @@ export default function ListItem(props){
                 <p>Added: {weekday} {month} {day}, {year} @ {time}</p>
             </div>
             <div className="individual-list-item-actions">
-                <div></div>
-                <div></div>
-                <div></div>
+                <Pencil></Pencil>
+                {
+                    complete ? 
+                    <CheckCircleFill className="complete-list-item" onClick={() => toggleCompletion(false)}></CheckCircleFill>
+                    :
+                    <div className="incomplete-list-item" onClick={() => toggleCompletion(true)}></div>
+                }
             </div>
         </div>
     )
