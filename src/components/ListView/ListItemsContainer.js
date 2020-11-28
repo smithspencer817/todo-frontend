88d647fe081097c6f8c470d3e038a80ba29d1fc3
smithspencer817 <smithspencer817@gmail.com>
2020-11-27 22:43:04 -0600

    added empty list default styling

--- src/components/ListView/ListItemsContainer.js
+++ src/components/ListView/ListItemsContainer.js
@@ -6,9 +6,16 @@ export default function ListItemsContainer(props) {
     return (
         <div id="home-page-list-container">
             {
-                props.listItems.map(listItem => 
-                    <ListItem listItem={listItem} key={listItem.id}/>
-                )
+                props.listItems.length > 0 ?
+                    props.listItems.map(listItem => 
+                        <ListItem listItem={listItem} key={listItem.id}/>
+                    )
+                :
+                <div className="list-item-empty-alert">
+                    <h1>LIST IS EMPTY</h1>
+                    <br></br>
+                    <h5>Click on 'Add Item' above to add new list items</h5>
+                </div>
             }
         </div>
     );
